a633dee36f2368d9c929bcbc0898afb7a31eaaea
akdlsz21 <akdlsz21@gmail.com>
2022-05-25 22:41:55 +0900

    shadowRoot added

--- tooltip.js
+++ tooltip.js
@@ -3,6 +3,7 @@ class Tooltip extends HTMLElement {
 		super();
 		this._tooltipContainer;
 		this._tooltipText = 'dummy text';
+		this.attachShadow({ mode: 'open' });
 	}
 
 	connectedCallback() {
@@ -16,7 +17,8 @@ class Tooltip extends HTMLElement {
 		tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
 		tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
 
-		this.appendChild(tooltipIcon);
+		this.style.position = 'relative';
+		this.shadowRoot.appendChild(tooltipIcon);
 	}
 
 	_showTooltip() {
@@ -25,11 +27,15 @@ class Tooltip extends HTMLElement {
 
 		this._tooltipContainer = document.createElement('div');
 		this._tooltipContainer.textContent = this._tooltipText;
-		this.appendChild(this._tooltipContainer);
+		this._tooltipContainer.style.backgroundColor = '#333';
+		this._tooltipContainer.style.color = 'white';
+		this._tooltipContainer.style.position = 'absolute';
+		this._tooltipContainer.style.zIndex = '10';
+		this.shadowRoot.appendChild(this._tooltipContainer);
 	}
 
 	_hideTooltip() {
-		this.removeChild(this._tooltipContainer);
+		this.shadowRoot.removeChild(this._tooltipContainer);
 	}
 }
 
