e89cdc7cce0e33e84bd58b7735e1a24677af6681
akdlsz21 <akdlsz21@gmail.com>
2022-05-25 22:20:18 +0900

    tooltip comp connectedCallback added

--- tooltip.js
+++ tooltip.js
@@ -1,8 +1,35 @@
 class Tooltip extends HTMLElement {
 	constructor() {
 		super();
+		this._tooltipContainer;
+		this._tooltipText = 'dummy text';
+	}
+
+	connectedCallback() {
+		// checks for attribute in html
+		if (this.hasAttribute('text'))
+			this._tooltipText = this.getAttribute('text');
+
 		const tooltipIcon = document.createElement('span');
 		tooltipIcon.textContent = ' (?) ';
+
+		tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+		tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+
+		this.appendChild(tooltipIcon);
+	}
+
+	_showTooltip() {
+		// convention for calling inside the class.
+		// private method
+
+		this._tooltipContainer = document.createElement('div');
+		this._tooltipContainer.textContent = this._tooltipText;
+		this.appendChild(this._tooltipContainer);
+	}
+
+	_hideTooltip() {
+		this.removeChild(this._tooltipContainer);
 	}
 }
 
